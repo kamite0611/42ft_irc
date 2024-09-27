59d2574d1ea717d8a1dc2560493622663bebf9fd
Kaistuc <kodaira-kai-vk@ynu.jp>
2024-09-27 17:44:25 +0900

    Commandのコンストラクタ完成

--- src/User/Command/Command.cpp
+++ src/User/Command/Command.cpp
@@ -14,12 +14,9 @@ _user(user), _server(server)
 	}
 
 	_parameters = irc::split(message, " ");
-	std::cout << "\n===== This is Command _parameters =====\n";
-	for (size_t i = 0; i < _parameters.size(); i++)
-		std::cout << _parameters[i] << std::endl;
-	// _prefix = _parameters[0];
-	// _parameters.erase(_parameters.begin());
+	_prefix = _parameters[0];
+	_parameters.erase(_parameters.begin());
 
-	// for (size_t i = 0; i < _prefix.length(); i++)
-	// 	_prefix[i] = std::toupper(_prefix[i]);
+	for (size_t i = 0; i < _prefix.length(); i++)
+		_prefix[i] = std::toupper(_prefix[i]);
 }
